444a6a409c3942a95ed3d17ee8ead9215d9a3dfe
Dominic <dominicdutchboy@gmail.com>
2024-07-26 14:48:36 +0200

    implemented: basic sessions configuration

---
+++ index.js
@@ -0,0 +1,26 @@
+const express = require("express")
+const dotenv = require("dotenv").config()
+const session = require("express-session")
+const app = express()
+
+const PORT = process.env.PORT || 4000
+const sessionKey = process.env.SESSIONKEY
+
+//Middleware to set cookies.
+app.use(session({
+    secret: sessionKey,
+    resave: false,
+    saveUninitialized: true,
+    cookie:{secure: false}
+}))
+
+app.get('/', (req, res) => res.status(200).json({message: "Home Page."}))
+
+//route to set new session.
+app.get('/cart', (req, res) => {
+    req.session.email = "dummymail@gmail.com"
+
+    res.status(200).json({message: `New session data: ${req.session.email}`});
+})
+
+app.listen(PORT, () => console.log(`Server is running on port ${PORT}`))
