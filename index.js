f527e4b7e9ddb7e89b58b578e9d00261354e915f
Dominic <dominicdutchboy@gmail.com>
2024-07-27 14:36:58 +0200

    implemented: set session store to a mongoDB cluster/collection

--- index.js
+++ index.js
@@ -1,23 +1,36 @@
-const express = require("express")
-const dotenv = require("dotenv").config()
-const cookie = require("cookie-parser")
-const session = require("express-session")
-const app = express()
+const express = require("express");
+const dotenv = require("dotenv").config();
+const cookie = require("cookie-parser");
+const session = require("express-session");
+const MongoStore = require("connect-mongo");
+const mongoose = require("mongoose")
+const app = express();
 
 const PORT = process.env.PORT || 4000
 
 const sessionKey = process.env.SESSIONKEY
 const cookieKey = process.env.COOKIEKEY
+const MONGOURL = process.env.MONGOURL
+
+mongoose.connect(MONGOURL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+}).then(()=>console.log("connected to mongodb")).catch((err)=>{console.error("unable to connect to mongodb cluster")})
 
 //Middleware to parse cookies with secret-key to the browser.
 app.use(cookie(cookieKey))
+
 
 //Middleware config to set cookies.
 app.use(session({
     secret: sessionKey,
     resave: false,
     saveUninitialized: true,
-    cookie:{secure: false}
+    cookie:{secure: false},
+    store: new MongoStore({
+        mongoUrl: MONGOURL,
+        collectionName: 'sessions'
+    })
 }))
 
 app.get('/', (req, res) => res.status(200).json({message: "Home Page."}))
@@ -26,9 +39,19 @@ app.get('/', (req, res) => res.status(200).json({message: "Home Page."}))
 app.get('/session', (req, res) => {
 
     //The session middleware creates a 'session' property in the request object.
+    req.session.user = {
+        UUID: '12234-2345-2323423',
+        name: "John Doe"
+    }
     req.session.email = "dummymail@gmail.com"
 
-    res.status(200).json({message: `New session data: ${req.session.email}`});
+    req.session.save((err) => {
+        if(err){
+            console.error(err)
+        } else {
+            res.status(200).json({message: `New session data: ${req.session.email}`});
+        }
+    })
 })
 
 //Route to set cookie.
